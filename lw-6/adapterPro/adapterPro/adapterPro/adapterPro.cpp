814297678a433ef79d704bd001cc558c140fbec5
nekosyakolya <greirex@mail.ru>
2018-03-27 23:12:02 +0300

    в библиотеки добавлен цвет

--- lw-6/adapterPro/adapterPro/adapterPro/adapterPro.cpp
+++ lw-6/adapterPro/adapterPro/adapterPro/adapterPro.cpp
@@ -18,6 +18,7 @@ namespace graphics_lib
 		virtual void MoveTo(int x, int y) = 0;
 		// Рисует линию с текущей позиции, передвигая перо в точку x,y 
 		virtual void LineTo(int x, int y) = 0;
+		virtual void SetColor(uint32_t rgbColor) = 0;
 		virtual ~ICanvas() = default;
 	};
 
@@ -32,6 +33,12 @@ namespace graphics_lib
 		void LineTo(int x, int y) override
 		{
 			cout << "LineTo (" << x << ", " << y << ")" << endl;
+		}
+		void SetColor(uint32_t rgbColor) override
+		{
+			// TODO: вывести в output цвет в виде строки SetColor (#RRGGBB)
+			cout << "color #" << std::hex << uppercase << rgbColor << std::dec << endl;
+
 		}
 	};
 }
@@ -49,7 +56,7 @@ namespace shape_drawing_lib
 	// Интерфейс объектов, которые могут быть нарисованы на холсте из graphics_lib
 	class ICanvasDrawable
 	{
-	public: 
+	public:
 		virtual void Draw(graphics_lib::ICanvas & canvas)const = 0;
 		virtual ~ICanvasDrawable() = default;
 	};
@@ -57,10 +64,11 @@ namespace shape_drawing_lib
 	class CTriangle : public ICanvasDrawable
 	{
 	public:
-		CTriangle(const Point & p1, const Point & p2, const Point & p3) :
+		CTriangle(const Point & p1, const Point & p2, const Point & p3, uint32_t rgbColor = 0x000000) :
 			m_point1(p1),
 			m_point2(p2),
-			m_point3(p3)
+			m_point3(p3),
+			m_rgbColor(rgbColor)
 		{
 			// TODO: написать код конструктора
 		}
@@ -68,6 +76,7 @@ namespace shape_drawing_lib
 		void Draw(graphics_lib::ICanvas & canvas)const override
 		{
 			// TODO: написать код рисования треугольника на холсте
+			canvas.SetColor(m_rgbColor);
 			canvas.MoveTo(m_point1.x, m_point1.y);
 
 			canvas.LineTo(m_point2.x, m_point2.y);
@@ -79,15 +88,17 @@ namespace shape_drawing_lib
 		Point m_point1;
 		Point m_point2;
 		Point m_point3;
+		uint32_t m_rgbColor;
 	};
 
 	class CRectangle : public ICanvasDrawable
 	{
 	public:
-		CRectangle(const Point & leftTop, int width, int height) :
+		CRectangle(const Point & leftTop, int width, int height, uint32_t rgbColor = 0x000000) :
 			m_leftTop(leftTop),
 			m_width(width),
-			m_height(height)
+			m_height(height),
+			m_rgbColor(rgbColor)
 		{
 			// TODO: написать код конструктора
 		}
@@ -95,6 +106,7 @@ namespace shape_drawing_lib
 		void Draw(graphics_lib::ICanvas & canvas)const override
 		{
 			// TODO: написать код рисования прямоугольника на холсте
+			canvas.SetColor(m_rgbColor);
 			canvas.MoveTo(m_leftTop.x, m_leftTop.y);
 
 			canvas.LineTo(m_leftTop.x, m_leftTop.y + m_height);
@@ -107,6 +119,7 @@ namespace shape_drawing_lib
 		Point m_leftTop;
 		int m_width;
 		int m_height;
+		uint32_t m_rgbColor;
 	};
 
 	// Художник, способный рисовать ICanvasDrawable-объекты на ICanvas
@@ -140,6 +153,13 @@ namespace modern_graphics_lib
 		int x;
 		int y;
 	};
+	// Цвет в формате RGBA, каждый компонент принимает значения от 0.0f до 1.0f
+	class CRGBAColor
+	{
+	public:
+		CRGBAColor(float r, float g, float b, float a) :r(r), g(g), b(b), a(a) {}
+		float r, g, b, a;
+	};
 
 	// Класс для современного рисования графики
 	class CModernGraphicsRenderer
@@ -169,13 +189,15 @@ namespace modern_graphics_lib
 		}
 
 		// Выполняет рисование линии
-		void DrawLine(const CPoint & start, const CPoint & end)
+		void DrawLine(const CPoint & start, const CPoint & end, const CRGBAColor& color)
 		{
 			if (!m_drawing)
 			{
 				throw logic_error("DrawLine is allowed between BeginDraw()/EndDraw() only");
 			}
-			m_out << boost::format(R"(<line fromX="%1%" fromY="%2%" toX="%3%" toY="%4%"/>)") % start.x % start.y % end.x % end.y << std::endl;
+			m_out << boost::format(R"(<line fromX="%1%" fromY="%2%" toX="%3%" toY="%4%">)") % start.x % start.y % end.x % end.y << std::endl;
+			m_out << '\t' << boost::format(R"(<color r="%1%" g="%2%" b="%3%" a="%4%" />)") % color.r % color.g % color.b % color.a << std::endl;
+			m_out << "</line>" << std::endl;
 		}
 
 		// Этот метод должен быть вызван в конце рисования
@@ -203,7 +225,8 @@ namespace app
 	public:
 		CModernGraphicsRendererAdapter(modern_graphics_lib::CModernGraphicsRenderer & render) :
 			m_render(render),
-			m_currentPoint(modern_graphics_lib::CPoint(0, 0))
+			m_currentPoint(modern_graphics_lib::CPoint(0, 0)),
+			m_rgbaColor(modern_graphics_lib::CRGBAColor(0, 0, 0, 1.0))
 		{
 			m_render.BeginDraw();
 		}
@@ -216,12 +239,19 @@ namespace app
 		void LineTo(int x, int y) override
 		{
 			modern_graphics_lib::CPoint endPoint(x, y);
-			m_render.DrawLine(m_currentPoint, endPoint);
+			m_render.DrawLine(m_currentPoint, endPoint, m_rgbaColor);
 
 			m_currentPoint = endPoint;
 		}
+		void SetColor(uint32_t rgbColor) override
+		{
+			m_rgbaColor.r = ((rgbColor >> 16) & 0xff) / 255.f;  // Extract the RR byte
+			m_rgbaColor.g = ((rgbColor >> 8) & 0xff) / 255.f;   // Extract the GG byte
+			m_rgbaColor.b = ((rgbColor) & 0xff) / 255.f;
+		}
 
 	private:
+		modern_graphics_lib::CRGBAColor m_rgbaColor;
 		modern_graphics_lib::CPoint m_currentPoint;
 		modern_graphics_lib::CModernGraphicsRenderer & m_render;
 	};
@@ -231,7 +261,7 @@ namespace app
 		using namespace shape_drawing_lib;
 
 		CTriangle triangle({ 10, 15 }, { 100, 200 }, { 150, 250 });
-		CRectangle rectangle({ 30, 40 }, 18, 24);
+		CRectangle rectangle({ 30, 40 }, 18, 24, 0x520057);
 
 		// TODO: нарисовать прямоугольник и треугольник при помощи painter
 
@@ -249,105 +279,13 @@ namespace app
 	void PaintPictureOnModernGraphicsRenderer()
 	{
 		modern_graphics_lib::CModernGraphicsRenderer renderer(cout);
-						 // TODO: при помощи существующей функции PaintPicture() нарисовать
-						 // картину на renderer
-						 // Подсказка: используйте паттерн "Адаптер"
+		// TODO: при помощи существующей функции PaintPicture() нарисовать
+		// картину на renderer
+		// Подсказка: используйте паттерн "Адаптер"
 		CModernGraphicsRendererAdapter adapter(renderer);
 		shape_drawing_lib::CCanvasPainter painter(adapter);
 		PaintPicture(painter);
 	}
-}
-
-namespace graphics_lib_pro
-{
-	// Холст для рисования
-	class ICanvas
-	{
-	public:
-		// Установка цвета в формате 0xRRGGBB
-		virtual void SetColor(uint32_t rgbColor) = 0;
-		virtual void MoveTo(int x, int y) = 0;
-		virtual void LineTo(int x, int y) = 0;
-		virtual ~ICanvas() = default;
-	};
-
-	// Реализация холста для рисования
-	class CCanvas : public ICanvas
-	{
-	public:
-		void SetColor(uint32_t rgbColor) override
-		{
-			// TODO: вывести в output цвет в виде строки SetColor (#RRGGBB)
-		}
-		void MoveTo(int x, int y) override
-		{
-			// Реализация остается без изменения
-		}
-		void LineTo(int x, int y) override
-		{
-			// Реализация остается без изменения
-		}
-	};
-}
-
-// Пространство имен обновленной современной графической библиотеки (недоступно для изменения)
-namespace modern_graphics_lib_pro
-{
-	class CPoint
-	{
-	public:
-		CPoint(int x, int y) :x(x), y(y) {}
-
-		int x;
-		int y;
-	};
-
-	// Цвет в формате RGBA, каждый компонент принимает значения от 0.0f до 1.0f
-	class CRGBAColor
-	{
-	public:
-		CRGBAColor(float r, float g, float b, float a) :r(r), g(g), b(b), a(a) {}
-		float r, g, b, a;
-	};
-
-	// Класс для современного рисования графики
-	class CModernGraphicsRenderer
-	{
-	public:
-		CModernGraphicsRenderer(ostream & strm) : m_out(strm)
-		{
-		}
-
-		~CModernGraphicsRenderer()
-		{
-			// Реализация остается без изменения
-		}
-
-		// Этот метод должен быть вызван в начале рисования
-		void BeginDraw()
-		{
-			// Реализация остается без изменения
-		}
-
-		// Выполняет рисование линии
-		void DrawLine(const CPoint & start, const CPoint & end, const CRGBAColor& color)
-		{
-			// TODO: выводит в output инструкцию для рисования линии в виде
-			// <line fromX="3" fromY="5" toX="5" toY="17">
-			//   <color r="0.35" g="0.47" b="1.0" a="1.0" />
-			// </line>
-			// Можно вызывать только между BeginDraw() и EndDraw()
-		}
-
-		// Этот метод должен быть вызван в конце рисования
-		void EndDraw()
-		{
-			// Реализация остается без изменения
-		}
-	private:
-		ostream & m_out;
-		bool m_drawing = false;
-	};
 }
 
 int main()
