c3f6220f75cc7f997706b6e9775a4fda3e30b249
nekosyakolya <greirex@mail.ru>
2018-05-04 09:56:25 +0300

    добавлены тесты фигур

--- lw-4/factory/tests/shapesTests.cpp
+++ lw-4/factory/tests/shapesTests.cpp
@@ -1,6 +1,11 @@
 ﻿#include "stdafx.h"
 #include "../factory/CEllipse.h"
 
+#include "../factory/CRectangle.h"
+
+#include "../factory/CRegularPolygon.h"
+#include "../factory/CTriangle.h"
+
 namespace
 {
 class CTestCanvas : public ICanvas
@@ -16,10 +21,9 @@ public:
 		m_color = color;
 		m_stream << "Color: " << m_color << std::endl;
 	}
-	void DrawLine(const Point& from, const Point& to) override
+	void DrawLine(const Point& /*from*/, const Point& /*to*/) override
 	{
-		m_stream << "From (" << from.x << ", " << from.y << ")" << std::endl;
-		m_stream << "To (" << to.x << ", " << to.y << ")" << std::endl;
+		m_stream << "DrawLine" << std::endl;
 	}
 	void DrawEllipse(float left, float top, float width, float height) override
 	{
@@ -72,16 +76,148 @@ BOOST_FIXTURE_TEST_SUITE(Ellipse, Ellipse_)
 
 	BOOST_AUTO_TEST_CASE(has_a_color)
 	{
-		BOOST_CHECK_EQUAL(ellipse.GetColor(), Color::GREEN);
+		BOOST_CHECK_EQUAL(ellipse.GetColor(), expectedColor);
 	}
 
 	BOOST_AUTO_TEST_CASE(can_be_drawn_on_canvas)
 	{
-		std::stringstream str;
-		CTestCanvas canvas(str);
+		std::stringstream strm;
+		CTestCanvas canvas(strm);
 		ellipse.Draw(canvas);
 
-		BOOST_CHECK_EQUAL(str.str(), "Color: green\nLeft top (130, 100)\nwidth:40\nheight:100\n");
+		BOOST_CHECK_EQUAL(strm.str(), "Color: green\nLeft top (130, 100)\nwidth:40\nheight:100\n");
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+namespace
+{
+struct Rectangle_
+{
+	const Point expectedLeftTop{ 10, 10 };
+	const Point expectedRightBottom{ 150, 150 };
+
+	const Color expectedColor = Color::PINK;
+	const CRectangle rectangle;
+	Rectangle_()
+		: rectangle(expectedLeftTop, expectedRightBottom, expectedColor)
+	{
+	}
+};
+
+} // namespace
+BOOST_FIXTURE_TEST_SUITE(Rectangle, Rectangle_)
+
+	BOOST_AUTO_TEST_CASE(has_a_leftTop)
+	{
+		BOOST_CHECK(rectangle.GetLeftTop() == expectedLeftTop);
+	}
+
+	BOOST_AUTO_TEST_CASE(has_a_rightBottom)
+	{
+		BOOST_CHECK(rectangle.GetRightBottom() == expectedRightBottom);
+	}
+
+	BOOST_AUTO_TEST_CASE(has_a_color)
+	{
+		BOOST_CHECK_EQUAL(rectangle.GetColor(), expectedColor);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_be_drawn_on_canvas)
+	{
+		std::stringstream strm;
+		CTestCanvas canvas(strm);
+		rectangle.Draw(canvas);
+
+		BOOST_CHECK_EQUAL(strm.str(), "Color: pink\nDrawLine\nDrawLine\nDrawLine\nDrawLine\n");
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+namespace
+{
+struct Triangle_
+{
+	const Point expectedFirst{ 10, 20 };
+	const Point expectedSecond{ 40, 80 };
+	const Point expectedThird{ 20, 40 };
+
+	const Color expectedColor = Color::PINK;
+	const CTriangle triangle;
+	Triangle_()
+		: triangle(expectedFirst, expectedSecond, expectedThird, expectedColor)
+	{
+	}
+};
+
+} // namespace
+
+BOOST_FIXTURE_TEST_SUITE(Triangle, Triangle_)
+
+	BOOST_AUTO_TEST_CASE(has_are_vertexes)
+	{
+		BOOST_CHECK(triangle.GetVertex1() == expectedFirst);
+		BOOST_CHECK(triangle.GetVertex2() == expectedSecond);
+		BOOST_CHECK(triangle.GetVertex3() == expectedThird);
+	}
+
+	BOOST_AUTO_TEST_CASE(has_a_color)
+	{
+		BOOST_CHECK_EQUAL(triangle.GetColor(), expectedColor);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_be_drawn_on_canvas)
+	{
+		std::stringstream strm;
+		CTestCanvas canvas(strm);
+		triangle.Draw(canvas);
+
+		BOOST_CHECK_EQUAL(strm.str(), "Color: pink\nDrawLine\nDrawLine\nDrawLine\n");
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+namespace
+{
+struct RegularPolygon_
+{
+	const Point expectedCenter{ 100, 100 };
+	const float expectedRadius = 40.f;
+	const size_t expectedVertexCount = 6;
+
+	const Color expectedColor = Color::PINK;
+	const CRegularPolygon regularPolygon;
+	RegularPolygon_()
+		: regularPolygon(expectedCenter, expectedRadius, expectedVertexCount, expectedColor)
+	{
+	}
+};
+
+} // namespace
+
+BOOST_FIXTURE_TEST_SUITE(RegularPolygon, RegularPolygon_)
+
+	BOOST_AUTO_TEST_CASE(has_a_center)
+	{
+		BOOST_CHECK(regularPolygon.GetCenter() == expectedCenter);
+	}
+
+	BOOST_AUTO_TEST_CASE(has_a_radius)
+	{
+		BOOST_CHECK_EQUAL(regularPolygon.GetRadius(), expectedRadius);
+	}
+	BOOST_AUTO_TEST_CASE(has_a_vertex_count)
+	{
+		BOOST_CHECK_EQUAL(regularPolygon.GetVertexCount(), expectedVertexCount);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_be_drawn_on_canvas)
+	{
+		std::stringstream strm;
+		CTestCanvas canvas(strm);
+		regularPolygon.Draw(canvas);
+
+		BOOST_CHECK_EQUAL(strm.str(), "Color: pink\nDrawLine\nDrawLine\nDrawLine\nDrawLine\nDrawLine\nDrawLine\n");
 	}
 
 BOOST_AUTO_TEST_SUITE_END()
