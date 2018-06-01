c021dc8103ead7567c2aff56fdb482dec7fffe00
nekosyakolya <greirex@mail.ru>
2018-06-01 04:00:00 +0300

    добавлены тесты

---
+++ lw-5/command/tests/ResizeImageTests.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+
+#include "../command/ResizeImage.h"
+
+struct ResizeImage_
+{
+	ResizeImage_()
+	{
+		width = 300;
+		height = 210;
+		newWidth = 600;
+		newHeight = 420;
+		command = std::make_unique<CResizeImage>(width, newWidth, height, newHeight);
+	}
+	int width;
+	int height;
+	int newWidth;
+	int newHeight;
+	std::unique_ptr<CResizeImage> command;
+};
+
+BOOST_FIXTURE_TEST_SUITE(Resize_image_command, ResizeImage_)
+
+	BOOST_AUTO_TEST_CASE(can_be_executed)
+	{
+		command->Execute();
+		BOOST_CHECK_EQUAL(width, newWidth);
+		BOOST_CHECK_EQUAL(height, newHeight);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_be_unexecuted)
+	{
+		command->Unexecute();
+		BOOST_CHECK(width != newWidth);
+		BOOST_CHECK(height != newHeight);
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
