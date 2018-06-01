0f96bcd22dc130a7779b0efa71b941eac2318fe7
nekosyakolya <greirex@mail.ru>
2018-06-01 04:22:10 +0300

    добавлены тесты

--- lw-5/command/tests/ResizeImageTests.cpp
+++ lw-5/command/tests/ResizeImageTests.cpp
@@ -30,6 +30,8 @@ BOOST_FIXTURE_TEST_SUITE(Resize_image_command, ResizeImage_)
 
 	BOOST_AUTO_TEST_CASE(can_be_unexecuted)
 	{
+		command->Execute();
+
 		command->Unexecute();
 		BOOST_CHECK(width != newWidth);
 		BOOST_CHECK(height != newHeight);
