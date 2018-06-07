089d44e433ea667290a02d98d86859a57f1db7f4
nekosyakolya <greirex@mail.ru>
2018-06-07 22:29:51 +0300

    добавлена мульти-версия

---
+++ lw-8/MultiGumballMachine/MultiGumballMachine/MultiGumBallMachineWithState.h
@@ -0,0 +1,135 @@
+﻿#pragma once
+
+namespace with_state
+{
+
+struct IState
+{
+	virtual void InsertCoin() = 0;
+	virtual void EjectCoin() = 0;
+	virtual void TurnCrank() = 0;
+	virtual void Dispense() = 0;
+	virtual std::string ToString() const = 0;
+	virtual ~IState() = default;
+};
+
+struct IGumballMachineContext
+{
+	virtual void ReleaseBall() = 0;
+	virtual size_t GetBallCount() const = 0;
+
+	virtual void SetSoldOutState() = 0;
+	virtual void SetNoCoinState() = 0;
+	virtual void SetSoldState() = 0;
+	virtual void SetHasCoinState() = 0;
+
+	virtual void AddCoin() = 0;
+	virtual void ReleaseCoin() = 0;
+	virtual size_t GetCoins() = 0;
+
+	virtual ~IGumballMachineContext() = default;
+};
+
+class CSoldState : public IState
+{
+public:
+	CSoldState(IGumballMachineContext& gumballMachine, std::ostream& out);
+
+	void InsertCoin() override;
+	void EjectCoin() override;
+	void TurnCrank() override;
+	void Dispense() override;
+	std::string ToString() const override;
+
+private:
+	IGumballMachineContext& m_gumballMachine;
+
+	std::ostream& m_out;
+};
+
+class CSoldOutState : public IState
+{
+public:
+	CSoldOutState(IGumballMachineContext& gumballMachine, std::ostream& out);
+
+	void InsertCoin() override;
+	void EjectCoin() override;
+	void TurnCrank() override;
+	void Dispense() override;
+	std::string ToString() const override;
+
+private:
+	IGumballMachineContext& m_gumballMachine;
+
+	std::ostream& m_out;
+};
+
+class CHasCoinState : public IState
+{
+public:
+	CHasCoinState(IGumballMachineContext& gumballMachine, std::ostream& out);
+
+	void InsertCoin() override;
+	void EjectCoin() override;
+	void TurnCrank() override;
+	void Dispense() override;
+	std::string ToString() const override;
+
+private:
+	IGumballMachineContext& m_gumballMachine;
+
+	std::ostream& m_out;
+};
+
+class CNoCoinState : public IState
+{
+public:
+	CNoCoinState(IGumballMachineContext& gumballMachine, std::ostream& out);
+
+	void InsertCoin() override;
+	void EjectCoin() override;
+	void TurnCrank() override;
+	void Dispense() override;
+	std::string ToString() const override;
+
+private:
+	IGumballMachineContext& m_gumballMachine;
+
+	std::ostream& m_out;
+};
+
+class CMultiGumballMachine : private IGumballMachineContext
+{
+public:
+	CMultiGumballMachine(size_t numBalls, std::ostream& out);
+
+	void EjectCoin();
+
+	void InsertCoin();
+	void TurnCrank();
+	std::string ToString() const;
+
+private:
+	size_t GetBallCount() const override;
+	void ReleaseBall() override;
+	void SetSoldOutState() override;
+	void SetNoCoinState() override;
+	void SetSoldState() override;
+	void SetHasCoinState() override;
+
+	size_t m_count = 0;
+	CSoldState m_soldState;
+	CSoldOutState m_soldOutState;
+	CNoCoinState m_noCoinState;
+	CHasCoinState m_hasCoinState;
+	IState* m_state;
+	std::ostream& m_out;
+
+	size_t m_coin = 0;
+	const size_t MAX_COUNT_OF_COINS = 5;
+
+	void AddCoin() override;
+	void ReleaseCoin() override;
+	size_t GetCoins() override;
+};
+} // namespace with_state
---
+++ lw-8/MultiGumballMachine/MultiGumballMachine/MultiGumballMachine.cpp
@@ -0,0 +1,230 @@
+﻿// GumballMachine.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "MultiGumBallMachineWithState.h"
+
+using namespace with_state;
+
+CSoldState::CSoldState(IGumballMachineContext& gumballMachine, std::ostream& out)
+	: m_gumballMachine(gumballMachine)
+	, m_out(out)
+{
+}
+void CSoldState::InsertCoin()
+{
+	m_out << "Please wait, we're already giving you a gumball\n";
+}
+void CSoldState::EjectCoin()
+{
+	m_out << "Sorry you already turned the crank\n";
+}
+void CSoldState::TurnCrank()
+{
+	m_out << "Turning twice doesn't get you another gumball\n";
+}
+void CSoldState::Dispense()
+{
+	m_gumballMachine.ReleaseBall();
+	m_gumballMachine.ReleaseCoin();
+
+	if (m_gumballMachine.GetCoins() > m_gumballMachine.GetBallCount())
+	{
+		m_gumballMachine.SetHasCoinState();
+	}
+	else if (m_gumballMachine.GetBallCount() == 0)
+	{
+		m_out << "Oops, out of gumballs\n";
+		m_gumballMachine.SetSoldOutState();
+	}
+	else
+	{
+		m_gumballMachine.SetNoCoinState();
+	}
+}
+std::string CSoldState::ToString() const
+{
+	return "delivering a gumball";
+}
+
+CSoldOutState::CSoldOutState(IGumballMachineContext& gumballMachine, std::ostream& out)
+	: m_gumballMachine(gumballMachine)
+	, m_out(out)
+{
+}
+
+void CSoldOutState::InsertCoin()
+{
+	m_out << "You can't insert a Coin, the machine is sold out\n";
+}
+void CSoldOutState::EjectCoin()
+{
+	m_out << "You can't eject, you haven't inserted a Coin yet\n";
+}
+void CSoldOutState::TurnCrank()
+{
+	m_out << "You turned but there's no gumballs\n";
+}
+void CSoldOutState::Dispense()
+{
+	m_out << "No gumball dispensed\n";
+}
+std::string CSoldOutState::ToString() const
+{
+	return "sold out";
+}
+
+CHasCoinState::CHasCoinState(IGumballMachineContext& gumballMachine, std::ostream& out)
+	: m_gumballMachine(gumballMachine)
+	, m_out(out)
+{
+}
+
+void CHasCoinState::InsertCoin()
+{
+	m_gumballMachine.AddCoin();
+}
+void CHasCoinState::EjectCoin()
+{
+	while (m_gumballMachine.GetCoins() != 0)
+	{
+		m_gumballMachine.ReleaseCoin();
+	}
+
+	m_gumballMachine.SetNoCoinState();
+}
+void CHasCoinState::TurnCrank()
+{
+	m_out << "You turned...\n";
+	m_gumballMachine.SetSoldState();
+}
+void CHasCoinState::Dispense()
+{
+	m_out << "No gumball dispensed\n";
+}
+std::string CHasCoinState::ToString() const
+{
+	return "waiting for turn of crank";
+}
+
+CNoCoinState::CNoCoinState(IGumballMachineContext& gumballMachine, std::ostream& out)
+	: m_gumballMachine(gumballMachine)
+	, m_out(out)
+{
+}
+
+void CNoCoinState::InsertCoin()
+{
+	m_gumballMachine.AddCoin();
+	m_gumballMachine.SetHasCoinState();
+}
+void CNoCoinState::EjectCoin()
+{
+	m_out << "You haven't inserted a Coin\n";
+}
+void CNoCoinState::TurnCrank()
+{
+	m_out << "You turned but there's no Coin\n";
+}
+void CNoCoinState::Dispense()
+{
+	m_out << "You need to pay first\n";
+}
+std::string CNoCoinState::ToString() const
+{
+	return "waiting for Coin";
+}
+
+CMultiGumballMachine::CMultiGumballMachine(size_t numBalls, std::ostream& out)
+	: m_soldState(*this, out)
+	, m_soldOutState(*this, out)
+	, m_noCoinState(*this, out)
+	, m_hasCoinState(*this, out)
+	, m_state(&m_soldOutState)
+	, m_count(numBalls)
+	, m_out(out)
+{
+	if (m_count > 0)
+	{
+		m_state = &m_noCoinState;
+	}
+}
+void CMultiGumballMachine::EjectCoin()
+{
+	m_state->EjectCoin();
+}
+void CMultiGumballMachine::InsertCoin()
+{
+	m_state->InsertCoin();
+}
+void CMultiGumballMachine::TurnCrank()
+{
+	m_state->TurnCrank();
+	m_state->Dispense();
+}
+std::string CMultiGumballMachine::ToString() const
+{
+	auto fmt = boost::format(R"(
+Mighty Gumball, Inc.
+C++-enabled Standing Gumball Model #2016 (with state)
+Inventory: %1% gumball%2%
+Machine is %3%
+)");
+	return (fmt % m_count % (m_count != 1 ? "s" : "") % m_state->ToString()).str();
+}
+
+size_t CMultiGumballMachine::GetBallCount() const
+{
+	return m_count;
+}
+void CMultiGumballMachine::ReleaseBall()
+{
+	if (m_count != 0)
+	{
+		m_out << "A gumball comes rolling out the slot...\n";
+		--m_count;
+	}
+}
+void CMultiGumballMachine::SetSoldOutState()
+{
+	m_state = &m_soldOutState;
+}
+void CMultiGumballMachine::SetNoCoinState()
+{
+	m_state = &m_noCoinState;
+}
+void CMultiGumballMachine::SetSoldState()
+{
+	m_state = &m_soldState;
+}
+void CMultiGumballMachine::SetHasCoinState()
+{
+	m_state = &m_hasCoinState;
+}
+
+void CMultiGumballMachine::AddCoin()
+{
+	if (m_coin != MAX_COUNT_OF_COINS)
+	{
+		++m_coin;
+		m_out << "You inserted a Coin\n";
+	}
+	else
+	{
+		m_out << "Can not insert coin\nStorage overflow!\n";
+	}
+}
+
+void CMultiGumballMachine::ReleaseCoin()
+{
+	if (m_coin != 0)
+	{
+		m_out << "Coin returned\n";
+		--m_coin;
+	}
+}
+
+size_t CMultiGumballMachine::GetCoins()
+{
+	return m_coin;
+}
